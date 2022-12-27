db2ee978c712ae11ac426c7abf93b5e7fe5cda04
danam <mdana@campus.technion.ac.il>
2022-12-27 19:44:19 +0200

    stam shomeret

--- main2.py
+++ main2.py
@@ -3,14 +3,6 @@ import numpy as np
 import pickle
 import time
 
-batch1_path = "./cifar-10-python/cifar-10-batches-py/data_batch_1"
-batch2_path = "./cifar-10-python/cifar-10-batches-py/data_batch_2"
-batch3_path = "./cifar-10-python/cifar-10-batches-py/data_batch_3"
-batch4_path = "./cifar-10-python/cifar-10-batches-py/data_batch_4"
-batch5_path = "./cifar-10-python/cifar-10-batches-py/data_batch_5"
-test_path = "./cifar-10-python/cifar-10-batches-py/test_batch"
-
-
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo,  encoding='bytes')
@@ -36,12 +28,47 @@ def getPCA(X, s):
     return Xs
 
 
-batch1_data = np.transpose(np.asarray(unpickle(batch1_path)[b'data']))
-batch2_data = np.transpose(np.asarray(unpickle(batch2_path)[b'data']))
-batch3_data = np.transpose(np.asarray(unpickle(batch3_path)[b'data']))
-batch4_data = np.transpose(np.asarray(unpickle(batch4_path)[b'data']))
-batch5_data = np.transpose(np.asarray(unpickle(batch5_path)[b'data']))
-test_data = np.transpose(np.asarray(unpickle(test_path)[b'data']))
+def turnGrey(A):
+    """
+    turning a color image (represented by a 1-D array)
+    to a greyscale image (also represented by a 1-D array)
+    """
+    final = np.zeros(A.shape[0], A.shape[1]/3)
+    for i in range(A.shape[0]):
+        img = A[i, :]
+        img_reshaped = np.transpose(np.reshape(img, (3, 32, 32)), (1, 2, 0))
+        actual_img = Image.fromarray(img_reshaped.astype('uint8'))
+        grey_img = actual_img.convert("L")
+        grey_img_mat = np.array(grey_img)
+        final[i, :] = grey_img_mat.flatten()
+    return final
 
-batches = [batch1_data, batch2_data, batch3_data, batch4_data, batch5_data, test_data]
-test1 = center(batch1_data)
+
+def gather_data(paths):
+    if len(paths) == 0:
+        return
+    if len(paths) == 1:
+        full_data = unpickle(paths)[b'data']
+        full_labels = unpickle(paths)[b'labels']
+    else:
+        full_data = unpickle(paths[0])[b'data']
+        full_labels = unpickle(paths[0])[b'labels']
+        for i in range(1, len(paths)):
+            batch_data = unpickle(paths[i])[b'data']
+            batch_labels = unpickle(paths[i])[b'labels']
+            full_labels = full_labels + batch_labels
+            full_data = np.concatenate((full_data, batch_data), axis=0)
+    return full_data, np.asarray(full_labels)
+
+
+if __name__ == '__main2__':
+    batch1_path = "./cifar-10-python/cifar-10-batches-py/data_batch_1"
+    batch2_path = "./cifar-10-python/cifar-10-batches-py/data_batch_2"
+    batch3_path = "./cifar-10-python/cifar-10-batches-py/data_batch_3"
+    batch4_path = "./cifar-10-python/cifar-10-batches-py/data_batch_4"
+    batch5_path = "./cifar-10-python/cifar-10-batches-py/data_batch_5"
+    test_path = "./cifar-10-python/cifar-10-batches-py/test_batch"
+    paths = [batch1_path, batch2_path, batch3_path, batch4_path, batch5_path]
+    tmp_data, labels = gather_data(paths)
+    data = turnGrey(tmp_data)
+
